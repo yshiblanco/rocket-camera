ce655dfbbbd0d4b41aa780cb4c4d0c587cf2e12c
yshiblanco <yshi.blanco@gmail.com>
2025-07-08 21:56:28 -0700

    testing sockets with the pi

--- app.c
+++ app.c
@@ -7,6 +7,8 @@
 #include <sys/un.h>
 
 #define SOCKET_PATH "/tmp/pi_socket"
+#define SOCKET_TYPE "SOCK_STREAM"
+#define DOMAIN      "AF_UNIX"
 #define SOCKET_TYPE SOCK_STREAM
 #define DOMAIN      AF_UNIX
 
@@ -29,4 +31,31 @@ int main(void) {
         printf("Failure with bind() call\n");
         return 1;
     }
+    //Listen for incoming connection requests (max of 1 in connection queue)
+    ret = listen(serverSocket, 1);
+    if (ret == -1) {
+        printf("Failure with listen() call\n");
+        return 1;
+    }
+    //Wait for a connection from a client
+    printf("Waiting for connection...\n");
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    clientSocket = accept(serverSocket, (struct sockaddr* ) &clientAddr, &clientAddrLen);
+    if (clientSocket == -1) {
+        printf("Failure with accept() call\n");
+        return 1;
+    }
+    printf("Connected to client\n");
+    while(1) {
+        char ch = 'H';
+        ssize_t bytesWritten = write(clientSocket, &ch, sizeof(ch));
+        if (bytesWritten <= 0) {
+            printf("Failure with write() call\n");
+            close(clientSocket);
+            close(serverSocket);
+            return 1;
+        }
+        sleep(5);
+    }
+    return 0;
 }
